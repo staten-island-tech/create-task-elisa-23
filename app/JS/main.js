fced69ef63ad40b0af8006b77bd195e67b8a278b
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2024-12-16 12:25:21 -0500

    asdfasfda

--- app/JS/main.js
+++ app/JS/main.js
@@ -3,9 +3,20 @@ import '../CSS/style.css';
 const DOMSelectors = {
     limited: document.querySelector("#limited"),
     standard: document.querySelector("#standard"),
-    weapon: document.querySelector("#weapon"),
     history: document.querySelector("#history"),
     container: document.querySelector("#container"),
     itemContainer: document.querySelector("#items")
 };
 
+let characters = [];
+let history = [];
+
+async function getData() {
+
+}
+
+function limited() {
+    DOMSelectors.limited("click", async function () {
+        DOMSelectors.container.className = "";
+    })
+}
