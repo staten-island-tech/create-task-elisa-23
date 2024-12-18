e6962af4be39ec323665fd6f80ceed1b7219545f
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2024-12-17 20:09:24 -0500

    j;lkjl

--- app/JS/main.js
+++ app/JS/main.js
@@ -11,22 +11,37 @@ const DOMSelectors = {
 let characters = [];
 let aquired = [];
 
-async function getData() {
-
-}
-
 function limited() {
     DOMSelectors.limited.addEventListener("click", async function () {
+        DOMSelectors.itemContainer.className = "";
         DOMSelectors.itemContainer.innerHTML = "";
         DOMSelectors.itemContainer.insertAdjacentHTML("beforeend",
             ``
-        )
+        );
+        //enter the rest of the code
     })
 }
 
 function history() {
     DOMSelectors.history.addEventListener("click", async function () {
-        DOMSelectors.itemContainer.innerHTML = "";
-
-    })
+        DOMSelectors.itemContainer.className = "grid grid-cols-4 gap-4";
+        DOMSelectors.itemContainer.innerHTML = ``;
+        aquired
+            .sort()
+            .forEach((character) => {
+                DOMSelectors.container.insertAdjacentHTML("beforeend",
+                    `<div class="card card-side justify-around shadow-xl w-[23rem] h-56 bg-white">
+                        <figure class="px-10">
+                            <img
+                                src="https://genshin.jmp.blue/characters/${character}/icon-big"
+                                alt="The icon of ${character}"
+                                class="rounded-xl w-full h-full object-cover" />
+                        </figure>
+                        <div class="card-body">
+                            <h2 class="card-title">${character}</h2>
+                        </div>
+                    </div>`
+                )
+            });
+    });
 }
